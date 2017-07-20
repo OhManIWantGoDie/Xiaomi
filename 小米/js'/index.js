6366ac41660036cacdf71cf1cf35032455117b99
OhManIWantGoDie <a836298587@163.com>
2017-07-20 21:13:15 +0800

    Xiaomi

---
+++ 小米/js'/index.js
@@ -0,0 +1,7 @@
+var mySwi = new Swiper(".swiper-container",{
+	loop:true,
+	autoplay:1000,
+	pagination:".swiper-pagination",
+	prevButton:'.swiper-button-prev',
+	nextButton:'.swiper-button-next'
+})
